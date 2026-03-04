0b2024924e0248468b2668616ef033b2df3b83d7
Harshit00yadav <hash00usr@gmail.com>
2026-03-04 18:38:10 +0530

    AVSU Testcase 1 complete

--- OrangeHRM-Testing/testing/src/test/java/com/orangehrm/testing/tests/adminviewsystemusers/TC_01.java
+++ OrangeHRM-Testing/testing/src/test/java/com/orangehrm/testing/tests/adminviewsystemusers/TC_01.java
@@ -1,14 +1,38 @@
 package com.orangehrm.testing.tests.adminviewsystemusers;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
 import com.orangehrm.testing.hooks.StartupTeardownhook;
+import com.orangehrm.testing.pom.AdminViewSystemUsers;
+import com.orangehrm.testing.pom.AuthLogin;
+import com.orangehrm.testing.pom.DashboardIndex;
 import com.orangehrm.testing.utils.ConfigLoader;
 
 public class TC_01 extends StartupTeardownhook {
-	@Test
-	public void initialize() {
-		String loginURL = ConfigLoader.getProperty("auth_login_url");
-		System.out.println("URL: " + loginURL);
+	private AuthLogin loginPage = new AuthLogin();
+	private DashboardIndex dashboardPage = new DashboardIndex();
+	private AdminViewSystemUsers adminPage = new AdminViewSystemUsers();
+
+	@Test(priority = 1)
+	public void loginWithCredentials() {
+		String username = ConfigLoader.getProperty("auth_username");
+		String password = ConfigLoader.getProperty("auth_password");
+		loginPage.navigateToAuthlogin();
+		loginPage.login(username, password);
+		assertTrue(dashboardPage.isDashboardVisible(), "Dashboard not visible, login failed");
+	}
+	
+	@Test(priority = 2)
+	public void isOnAdminPage() {
+		adminPage.navigateToAdminview();
+		assertTrue(adminPage.isAdminHeaderVisible(), "Admin page header not visible");
+	}
+	
+	@Test(priority = 3)
+	public void recordsCountMatching() {
+		assertEquals(adminPage.getRecordsCount(), adminPage.getUserCards().size(), "Count Mismatch");
 	}
 }
